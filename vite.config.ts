3541d79c3863adbed54b2961b1cc830d0121b12a
lexxv <mister.alex49@yandex.ru>
2024-04-06 23:12:21 +0300

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,42 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
+import {VitePWA} from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3000,
-  },
-  base:'/',
-  optimizeDeps: {
-    exclude: ['@changey/react-leaflet-markercluster']
-  }
+    plugins: [
+        react(),
+        VitePWA({
+            registerType: 'autoUpdate',
+            // devOptions: {
+            //     enabled: true,
+            // },
+            workbox: {
+                clientsClaim: true,
+                skipWaiting: true,
+                globPatterns: ['**/*.{js,jsx,ts,tsx,css,html,ico,png,svg}']
+            },
+            injectRegister: 'auto',
+            includeAssets: ['vite.svg', 'apple-touch-icon.png'],
+            manifest: {
+                name: 'На колёсах!',
+                short_name: 'НАК',
+                description: 'My Awesome App description',
+                theme_color: '#ffffff',
+                icons: [
+                    {
+                        src: 'apple-touch-icon.png',
+                        sizes: '180x180',
+                        type: 'image/png'
+                    },
+                ]
+            }
+        }),
+    ],
+    server: {
+        port: 3000,
+    },
+    base: '/',
+    optimizeDeps: {
+        exclude: ['@changey/react-leaflet-markercluster'],
+    },
 })
